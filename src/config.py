a6f152f0a28c41296f876a5e970980e836de1ae0
Livington <lopezcruzlivingtonlenin@gmail.com>
2024-07-14 19:26:56 -0500

    First Commit

---
+++ src/config.py
@@ -0,0 +1,22 @@
+WIDTH  :int = 0
+HEIGHT :int = 0
+
+WIDTH_TABLE :int = 0
+HEIGHT_TABLE :int = 0
+
+SIZE_CUBE :int = 0
+
+COLUMN_TABLE :int = WIDTH_TABLE//SIZE_CUBE
+ROW_TABLE :int = HEIGHT_TABLE//SIZE_CUBE
+
+#logo
+ICO_GAME :str = "assets/image/logo/snakegame_logo.ico"
+LOGO_GAME :str = "assets/image/logo/snakegame_logo.png"
+
+#buttons
+DIRIMAGE_BUTTON_CONTINUE :str = "assets/image/buttons/continue.png"
+DIRIMAGE_BUTTON_PAUSE :str = "assets/image/buttons/pause.png"
+DIRIMAGE_BUTTON_RETRY :str = "assets/image/buttons/retry.png"
+
+BLACK :tuple = (0,0,0)
+WHITE :tuple = (255,255,255)
---
+++ src/resourcesgame.py
@@ -0,0 +1,2 @@
+class ResourcesGame:
+    pass
